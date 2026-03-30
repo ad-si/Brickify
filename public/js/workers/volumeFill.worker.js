e56314fa7a37c7f1eaf7c549a2f0212d1defc1fc
Adrian Sieber <mail@adriansieber.com>
2026-03-30 15:34:53 +0000

    Fix CI: add missing @popperjs/core dependency and trigger on develop branch
      - Add @popperjs/core as explicit dependency (required by bootstrap, broke build-static)
      - Add develop branch to test workflow triggers
      - Remove redundant text-encoding.mjs shim (superseded by .ts version)
      - Regenerate worker build output

--- public/js/workers/volumeFill.worker.js
+++ public/js/workers/volumeFill.worker.js
@@ -1,105 +1,86 @@
-const VolumeFillWorker = {
-  lastProgress: -1,
-  fillGrid(grid, callback) {
-    let voxelColumn;
-    let voxelPlane;
-    let x;
-    let y;
-    const numVoxelsX = grid.length - 1;
-    let numVoxelsY = 0;
-    let numVoxelsZ = 0;
-    for (x in grid) {
-      voxelPlane = grid[x];
-      numVoxelsY = Math.max(numVoxelsY, voxelPlane.length - 1);
-      for (y in voxelPlane) {
-        voxelColumn = voxelPlane[y];
-        numVoxelsZ = Math.max(numVoxelsZ, voxelColumn.length - 1);
-      }
-    }
-    this._resetProgress();
-    for (x in grid) {
-      voxelPlane = grid[x];
-      const xNum = parseInt(x);
-      for (y in voxelPlane) {
-        voxelColumn = voxelPlane[y];
-        const yNum = parseInt(y);
-        this._postProgress(callback, xNum, yNum, numVoxelsX, numVoxelsY);
-        this._fillUp(grid, xNum, yNum, numVoxelsZ);
-      }
-    }
-    return callback({ state: "finished", data: grid });
-  },
-  // _fillUp: (grid, x, y, numVoxelsZ)
-  _fillUp(grid, x, y, numVoxelsZ) {
-    let insideModel = false;
-    let z = 0;
-    const currentFillVoxelQueue = [];
-    while (z <= numVoxelsZ) {
-      if (grid[x][y][z] != null) {
-        const voxelData = grid[x][y][z];
-        const dir = voxelData.dir;
-        this._setVoxels(grid, x, y, currentFillVoxelQueue, 0);
-        if (dir === 1) {
-          insideModel = false;
-        } else if (dir === -1) {
-          insideModel = true;
-        }
-      } else {
-        if (insideModel) {
-          currentFillVoxelQueue.push(z);
-        }
-      }
-      z++;
-    }
-  },
-  _setVoxels(grid, x, y, zValues, voxelData) {
-    return (() => {
-      let zValue;
-      const result = [];
-      while (zValue = zValues.pop()) {
-        result.push(this._setVoxel(grid, x, y, zValue, voxelData));
-      }
-      return result;
-    })();
-  },
-  _setVoxel(grid, x, y, z, voxelData) {
-    if (grid[x] == null) {
-      grid[x] = [];
-    }
-    if (grid[x][y] == null) {
-      grid[x][y] = [];
-    }
-    if (grid[x][y][z] == null) {
-      grid[x][y][z] = void 0;
-    }
-    grid[x][y][z] = voxelData;
-  },
-  _resetProgress() {
-    this.lastProgress = -1;
-  },
-  _postProgress(callback, x, y, numVoxelsX, numVoxelsY) {
-    const progress = Math.round(
-      100 * ((x - 1) * numVoxelsY + y - 1) / numVoxelsX / numVoxelsY
-    );
-    if (!(progress > this.lastProgress)) {
-      return;
-    }
-    this.lastProgress = progress;
-    callback({ state: "progress", progress });
-  }
+//#region src/plugins/newBrickator/pipeline/voxelization/VolumeFillWorker.ts
+var VolumeFillWorker = {
+	lastProgress: -1,
+	fillGrid(grid, callback) {
+		const numVoxelsX = grid.length - 1;
+		let numVoxelsY = 0;
+		let numVoxelsZ = 0;
+		for (const voxelPlane of grid) {
+			numVoxelsY = Math.max(numVoxelsY, voxelPlane.length - 1);
+			for (const voxelColumn of voxelPlane) numVoxelsZ = Math.max(numVoxelsZ, voxelColumn.length - 1);
+		}
+		this._resetProgress();
+		for (let xNum = 0; xNum < grid.length; xNum++) {
+			const voxelPlane = grid[xNum];
+			for (let yNum = 0; yNum < voxelPlane.length; yNum++) {
+				this._postProgress(callback, xNum, yNum, numVoxelsX, numVoxelsY);
+				this._fillUp(grid, xNum, yNum, numVoxelsZ);
+			}
+		}
+		callback({
+			state: "finished",
+			data: grid
+		});
+		return {
+			state: "finished",
+			data: grid
+		};
+	},
+	_fillUp(grid, x, y, numVoxelsZ) {
+		let insideModel = false;
+		let z = 0;
+		const currentFillVoxelQueue = [];
+		while (z <= numVoxelsZ) {
+			if (grid[x][y][z] != null) {
+				const dir = grid[x][y][z].dir;
+				this._setVoxels(grid, x, y, currentFillVoxelQueue, 0);
+				if (dir === 1) insideModel = false;
+				else if (dir === -1) insideModel = true;
+			} else if (insideModel) currentFillVoxelQueue.push(z);
+			z++;
+		}
+	},
+	_setVoxels(grid, x, y, zValues, voxelData) {
+		let zValue;
+		while ((zValue = zValues.pop()) !== void 0) this._setVoxel(grid, x, y, zValue, voxelData);
+	},
+	_setVoxel(grid, x, y, z, voxelData) {
+		if (grid[x] == null) grid[x] = [];
+		if (grid[x][y] == null) grid[x][y] = [];
+		if (grid[x][y][z] == null) grid[x][y][z] = void 0;
+		grid[x][y][z] = voxelData;
+	},
+	_resetProgress() {
+		this.lastProgress = -1;
+	},
+	_postProgress(callback, x, y, numVoxelsX, numVoxelsY) {
+		const progress = Math.round(100 * ((x - 1) * numVoxelsY + y - 1) / numVoxelsX / numVoxelsY);
+		if (!(progress > this.lastProgress)) return;
+		this.lastProgress = progress;
+		callback({
+			state: "progress",
+			progress
+		});
+	}
 };
+//#endregion
+//#region src/plugins/newBrickator/pipeline/voxelization/volumeFill.worker.ts
 self.onmessage = (event) => {
-  const { type, data } = event.data || {};
-  if (type === "fillGrid") {
-    const { gridPOJO } = data;
-    const callback = (message) => {
-      self.postMessage(message);
-    };
-    try {
-      VolumeFillWorker.fillGrid(gridPOJO, callback);
-    } catch (err) {
-      const errorMessage = err instanceof Error ? err.message : String(err);
-      self.postMessage({ state: "error", error: errorMessage });
-    }
-  }
+	const { type, data } = event.data;
+	if (type === "fillGrid") {
+		const { gridPOJO } = data;
+		const callback = (message) => {
+			self.postMessage(message);
+		};
+		try {
+			VolumeFillWorker.fillGrid(gridPOJO, callback);
+		} catch (err) {
+			const errorMessage = err instanceof Error ? err.message : String(err);
+			self.postMessage({
+				state: "error",
+				error: errorMessage
+			});
+		}
+	}
 };
+//#endregion
--- src/shims/text-encoding.mjs
+++
@@ -1,5 +0,0 @@
-// Shim for the 'text-encoding' npm package.
-// Modern browsers ship native TextEncoder/TextDecoder,
-// so the heavy polyfill (which crashes under strict-mode bundling) is unnecessary.
-export { TextEncoder, TextDecoder }
-export default { TextEncoder, TextDecoder }
